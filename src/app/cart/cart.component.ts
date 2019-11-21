6904a45bf78bc3540853bee19cfef8325aa605cc
Admin <admin@Admins-MacBook-Pro.local>
2019-11-20 16:37:43 -0800

    added form fields

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -5,22 +5,44 @@ import { Component, OnInit } from '@angular/core';
 // A service has to be "exported" for it to be "imported"
 import { CartService } from './../cart.service';
 
+//The FormBuilder service is provided by the ReactiveFormsModule, which is already defined in the AppModule you modified previously (in app.module.ts).
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit{
+export class CartComponent {
 
   items;
+  // Define the checkoutForm property to store the form model
+  checkoutForm;
 
   constructor(
 
-    private cartService: CartService
+    private cartService: CartService,
+    //inject the FormBuilder service
+    private formBuilder: FormBuilder,
 
-  ) { }
+  ) {
 
-  ngOnInit(){
     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+
   }
+
+  onSubmit(customerData) {
+
+    //Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+  }
+
 }
