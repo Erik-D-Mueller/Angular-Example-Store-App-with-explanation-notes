91e936edaa41485bcc800435f944e4c952fe08fd
Admin <admin@Admins-MacBook-Pro.local>
2019-12-03 12:03:11 -0800

    functionally finished, still have some notes to add in for instruction

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
 import { products } from '../products';
 
 /* Notice the @Component() decorator. This indicates that the following class is a component.
 It provides metadata about the component, including its selector, templates, and styles.
 The selector identifies the component. The selector is the name you give the Angular component
-when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app-, followed by the component name.
+when it is rendered as an HTML element on the page. By convention, Angular component selectors
+begin with the prefix app-, followed by the component name.
 The template and style filenames reference the HTML and CSS files that StackBlitz generates.
 The component definition also exports the class, ProductAlertsComponent, which handles functionality
  for the component. */
@@ -28,4 +28,3 @@ export class ProductListComponent {
   }
 
 }
-
